ebfe51152096946978c338322682c9a16ee7249b
Batuhan Ates <batuhanates04@gmail.com>
2020-05-03 17:11:06 +0300

    Add comments to sc datapath testbench

--- sim/testbench/panda_sc_datapath_tb.sv
+++ sim/testbench/panda_sc_datapath_tb.sv
@@ -29,7 +29,7 @@ module panda_sc_datapath_tb ();
   logic [31:0] jump_target_o;
   logic        branch_cond_o;
 
-  panda_pkg::alu_operator_e alu_operator_i = ALU_ADD;
+  alu_operator_e alu_operator_i = ALU_ADD;
 
   panda_sc_datapath dut (
     .clk_i             (clk_i             ),
@@ -58,6 +58,7 @@ module panda_sc_datapath_tb ();
 
   always #5 clk_i = ~clk_i;
 
+  // increment pc (ignore branch and jumps)
   always_ff @(posedge clk_i or negedge rst_ni) begin : proc_pc
     if(~rst_ni) begin
       pc_i <= 0;
@@ -70,20 +71,31 @@ module panda_sc_datapath_tb ();
 
   initial begin : proc_stim
     #10 rst_ni = 1'b1;
+    // ADDI x1, x0, 10
     rd_addr_i = 1; rd_we_i = 1'b1; imm_i = 10; sel_operand_b_i = 1'b1;
+    // ADDI x2, x0, 15
     #10 rd_addr_i = 2; imm_i = 15;
+    // JAL x3, 12
     #10 rd_addr_i = 3; sel_rd_data_i = 2'b10;
     sel_operand_a_i = 1'b1; sel_operand_b_i = 1'b1; imm_i = 12;
+    // ADDI x4, x1, 24
     #10 rd_addr_i = 4; sel_operand_a_i = 1'b0; rs1_addr_i = 1; imm_i = 24;
+    // BGE x4, x3, 24
     #10 rd_we_i = 1'b0; rs1_addr_i = 4; rs2_addr_i = 3;
     sel_operand_a_i = 1'b0; sel_operand_b_i = 1'b0; alu_operator_i = ALU_GE;
+    // BLT x4, x3, 24
     #10 alu_operator_i = ALU_LT;
+    // SW x0, x1, 16
     #10 rs1_addr_i = 0; rs2_addr_i = 1; sel_operand_b_i = 1'b1; imm_i = 16;
     load_store_i = 1'b1; load_store_width_i = 2'b10; alu_operator_i = ALU_ADD;
+    // SH x0, x1, 20
     #10 imm_i = 20; load_store_width_i = 2'b01;
+    // SH x0, x1, 22
     #10 imm_i = 22;
+    // LH x5, x0, 12
     #10 rd_we_i = 1'b1; rd_addr_i = 5; load_store_i = 1'b0; imm_i = 12;
     sel_rd_data_i = 2'b01; data_rdata_i = 32'hABCDEF78;
+    // LHU x6, x0, 12
     #10 rd_addr_i = 6; load_unsigned_i = 1'b1;
     #10 $finish;
   end
