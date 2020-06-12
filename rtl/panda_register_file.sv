81f24379a6d6cae8c9b902d39c6f3a6034e1d310
Batuhan Ates <batuhanates04@gmail.com>
2020-06-12 20:42:08 +0300

    Update register file for write-first operating

--- rtl/panda_register_file.sv
+++ rtl/panda_register_file.sv
@@ -23,16 +23,18 @@ module panda_register_file #(
   logic [Depth-1:0][Width-1:0] registers;
   logic [Depth-1:1][Width-1:0] registers_tmp;
   logic [Depth-1:0]            we;
+  logic [Depth-1:0]            we_tmp;
 
   // Register x0 is hardwired with all bits equal to 0
   assign registers[0]         = '0;
   assign registers[Depth-1:1] = registers_tmp;
 
-  assign rs1_data_o = registers[rs1_addr_i];
-  assign rs2_data_o = registers[rs2_addr_i];
-
   // decoder
-  assign we = ({{Depth-1{1'b0}}, 1'b1} << rd_addr_i) & {Depth{rd_we_i}};
+  assign we_tmp = ({{Depth-1{1'b0}}, 1'b1} << rd_addr_i) & {Depth{rd_we_i}};
+  assign we     = {we_tmp[Depth-1:1], 1'b0};
+
+  assign rs1_data_o = we[rs1_addr_i] ? rd_data_i : registers[rs1_addr_i];
+  assign rs2_data_o = we[rs2_addr_i] ? rd_data_i : registers[rs2_addr_i];
 
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (~rst_ni) begin
