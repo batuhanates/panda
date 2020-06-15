a72e00f2646db5050e824e73863b9d535f8c108f
Batuhan Ates <batuhanates04@gmail.com>
2020-06-15 20:00:34 +0300

    Update core module

--- rtl/panda_core.sv
+++ rtl/panda_core.sv
@@ -21,56 +21,51 @@ module panda_core (
   ex_mem_t ex_mem;
   mem_wb_t mem_wb;
 
-  logic [31:0] jump_target;
-  logic [31:0] branch_target;
-  logic        branch_cond;
-  logic        branch;
-
   logic [31:0] rd_data;
+  logic [31:0] rd_data_ex;
 
   logic load_use_hazard;
 
   logic stall_if;
-  logic bubble_id;
-
-  assign branch = id_ex.branch & branch_cond;
+  logic flush_if;
 
   panda_if_stage i_if_stage (
-    .clk_i          (clk_i        ),
-    .rst_ni         (rst_ni       ),
-    .stall_i        (stall_if     ),
-    .if_id_o        (if_id        ),
-    .instr_rdata_i  (instr_rdata_i),
-    .instr_addr_o   (instr_addr_o ),
-    .branch_i       (branch       ),
-    .jump_i         (id_ex.jump   ),
-    .branch_target_i(branch_target),
-    .jump_target_i  (jump_target  )
+    .clk_i        (clk_i            ),
+    .rst_ni       (rst_ni           ),
+    .stall_i      (stall_if         ),
+    .flush_i      (flush_if         ),
+    .if_id_o      (if_id            ),
+    .instr_rdata_i(instr_rdata_i    ),
+    .instr_addr_o (instr_addr_o     ),
+    .change_flow_i(id_ex.change_flow),
+    .jb_address_i (id_ex.jb_address )
   );
 
   panda_id_stage i_id_stage (
-    .clk_i            (clk_i          ),
-    .rst_ni           (rst_ni         ),
-    .bubble_i         (bubble_id      ),
-    .if_id_i          (if_id          ),
-    .id_ex_o          (id_ex          ),
-    .rd_data_i        (rd_data        ),
-    .rd_addr_i        (mem_wb.rd_addr ),
-    .rd_we_i          (mem_wb.rd_we   ),
-    .load_use_hazard_o(load_use_hazard)
+    .clk_i               (clk_i             ),
+    .rst_ni              (rst_ni            ),
+    .if_id_i             (if_id             ),
+    .id_ex_o             (id_ex             ),
+    .rd_data_i           (rd_data           ),
+    .rd_addr_i           (mem_wb.rd_addr    ),
+    .rd_we_i             (mem_wb.rd_we      ),
+    .ex_mem_rd_data_sel_i(ex_mem.rd_data_sel),
+    .ex_mem_rd_addr_i    (ex_mem.rd_addr    ),
+    .ex_mem_rd_we_i      (ex_mem.rd_we      ),
+    .rd_data_ex_i        (rd_data_ex        ),
+    .stall_if_o          (stall_if          ),
+    .flush_if_o          (flush_if          )
   );
 
   panda_ex_stage i_ex_stage (
-    .clk_i          (clk_i         ),
-    .rst_ni         (rst_ni        ),
-    .id_ex_i        (id_ex         ),
-    .ex_mem_o       (ex_mem        ),
-    .jump_target_o  (jump_target   ),
-    .branch_target_o(branch_target ),
-    .branch_cond_o  (branch_cond   ),
-    .rd_addr_i      (mem_wb.rd_addr),
-    .rd_we_i        (mem_wb.rd_we  ),
-    .rd_data_i      (rd_data       )
+    .clk_i       (clk_i         ),
+    .rst_ni      (rst_ni        ),
+    .id_ex_i     (id_ex         ),
+    .ex_mem_o    (ex_mem        ),
+    .rd_addr_i   (mem_wb.rd_addr),
+    .rd_we_i     (mem_wb.rd_we  ),
+    .rd_data_i   (rd_data       ),
+    .rd_data_ex_o(rd_data_ex    )
   );
 
   panda_mem_stage i_mem_stage (
@@ -93,11 +88,5 @@ module panda_core (
       default        : rd_data = mem_wb.alu_result;
     endcase
   end
-
-  panda_controller i_controller (
-    .load_use_hazard_i(load_use_hazard),
-    .stall_if_o       (stall_if       ),
-    .bubble_id_o      (bubble_id      )
-  );
 
 endmodule
