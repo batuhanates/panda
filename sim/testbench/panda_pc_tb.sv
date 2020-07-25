ea750616532ad09043959da0676b891f3c72c8dc
Batuhan Ates <batuhanates04@gmail.com>
2020-07-25 14:49:28 +0300

    Update program counter testbench

--- sim/testbench/panda_pc_tb.sv
+++ sim/testbench/panda_pc_tb.sv
@@ -8,39 +8,36 @@ module panda_pc_tb ();
 
   parameter int unsigned Width = 32;
 
-  logic clk    = 1'b0;
-  logic rst_n  = 1'b0;
-  logic branch = 1'b0;
-  logic jump   = 1'b0;
+  logic clk         = 1'b0;
+  logic rst_n       = 1'b0;
+  logic stall       = 1'b0;
+  logic change_flow = 1'b0;
 
-  logic [Width-1:0] branch_target = 0;
-  logic [Width-1:0] jump_target   = 0;
+  logic [Width-1:0] target_address = 0;
   logic [Width-1:0] pc;
   logic [Width-1:0] pc_inc;
 
   panda_pc #(
     .Width(Width)
   ) dut (
-    .clk_i          (clk          ),
-    .rst_ni         (rst_n        ),
-    .branch_i       (branch       ),
-    .jump_i         (jump         ),
-    .branch_target_i(branch_target),
-    .jump_target_i  (jump_target  ),
-    .pc_o           (pc           ),
-    .pc_inc_o       (pc_inc       )
+    .clk_i           (clk           ),
+    .rst_ni          (rst_n         ),
+    .stall_i         (stall         ),
+    .change_flow_i   (change_flow   ),
+    .target_address_i(target_address),
+    .pc_o            (pc            ),
+    .pc_inc_o        (pc_inc        )
   );
 
   always #5 clk = ~clk;
 
   initial begin : proc_stim
     #10 rst_n = 1'b1;
-    branch_target = 24;
-    jump_target = 56;
-    #30 branch = 1'b1;
-    #10 branch = 1'b0;
-    #30 jump = 1'b1;
-    #10 jump = 1'b0;
+    target_address = 24;
+    #30 change_flow = 1'b1;
+    #10 change_flow = 1'b0;
+    #30 stall = 1'b1;
+    #10 stall = 1'b0;
     #35 $finish;
   end
 
